fdca855a23ebab1342eaf2c99719832ba396ca76
Beyang Liu <beyang@sourcegraph.com>
2018-12-15 11:24:28 -0800

    initialize

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/RemoteLanguageServer.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/RemoteLanguageServer.java
@@ -14,6 +14,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -47,14 +49,14 @@ public class RemoteLanguageServer implements LanguageServer, TextDocumentService
 	private JDTLanguageServer underlying;
 
 	// cacheRoot is the root directory of the local cache of source files.
-	private File cacheRoot;
+	private File cacheContainer;
 
 	private ConcurrentMap<String, String> uriToPath;
 	private ConcurrentMap<String, String> pathToUri;
 
-	public RemoteLanguageServer(JDTLanguageServer underlying, File cacheRoot) {
+	public RemoteLanguageServer(JDTLanguageServer underlying, File cacheContainer) {
 		this.underlying = underlying;
-		this.cacheRoot = cacheRoot;
+		this.cacheContainer = cacheContainer;
 		this.uriToPath = new ConcurrentHashMap<>();
 		this.pathToUri = new ConcurrentHashMap<>();
 	}
@@ -62,10 +64,18 @@ public class RemoteLanguageServer implements LanguageServer, TextDocumentService
 	private String uriToCachePath(String uri) {
 		try {
 			URL url = new URL(uri);
-			return Paths.get(cacheRoot.toPath().toString(), url.getHost(), url.getPath().replace("/", File.separator)).toString();
+			return Paths.get(cacheRootDir(), url.getHost(), url.getPath().replace("/", File.separator)).toString();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private String cacheTmpDir() {
+		return Paths.get(cacheContainer.toString(), "tmp").toString();
+	}
+
+	private String cacheRootDir() {
+		return Paths.get(cacheContainer.toString(), "root").toString();
 	}
 
 	@Override
@@ -73,26 +83,36 @@ public class RemoteLanguageServer implements LanguageServer, TextDocumentService
 		try {
 			String rootUri = params.getRootUri();
 			String rootCachePath = uriToCachePath(rootUri);
-			this.uriToPath.put(rootUri, rootCachePath);
-			this.pathToUri.put(rootCachePath, rootUri);
 
-			// TODO: try-with-resources block (so things are closed properly)
-			InputStream respBody = HTTPUtils.httpGet("http://localhost:3080/github.com/gorilla/mux/-/raw");
+			if (!uriToPath.containsKey(rootUri)) {
+				// TODO(beyang): make async
+				fetchTree(rootUri, rootCachePath);
+				this.pathToUri.put(rootCachePath, rootUri);
+				this.uriToPath.put(rootUri, rootCachePath);
+			}
 
-			// TODO: unzip to temp folder, then move to rootCachePath
+			return underlying.initialize(params);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-			//			String respBodyString = IOUtils.toString(respBody, "UTF-8");
-			//			System.err.println("################");
-			//			System.err.println(respBodyString);
+	private void fetchTree(String remoteUri, String localPath) {
+		// TODO(beyang): try-with-resources block (so things are closed properly)
+		try {
+			InputStream respBody = HTTPUtils.httpGet(remoteUri);
+			new File(cacheTmpDir()).mkdirs();
+			Path tmpDir = Files.createTempDirectory(Paths.get(cacheTmpDir()), Paths.get(localPath).getFileName().toString());
+			// TODO(beyang): delete tmpDir if still exists
 
 			ZipInputStream zipIn = new ZipInputStream(respBody);
 			byte[] buffer = new byte[1024];
 			for (ZipEntry entry = zipIn.getNextEntry(); entry != null; entry = zipIn.getNextEntry()) {
 				if (entry.isDirectory()) {
-					new File(rootCachePath + File.separator + entry.getName()).mkdirs();
+					new File(tmpDir + File.separator + entry.getName()).mkdirs();
 					continue;
 				}
-				File newFile = new File(rootCachePath + File.separator + entry.getName());
+				File newFile = new File(tmpDir + File.separator + entry.getName());
 				new File(newFile.getParent()).mkdirs();
 				FileOutputStream fos = new FileOutputStream(newFile);
 				int len;
@@ -102,7 +122,8 @@ public class RemoteLanguageServer implements LanguageServer, TextDocumentService
 				fos.close();
 			}
 
-			return underlying.initialize(params);
+			new File(new File(localPath).getParent()).mkdirs();
+			Files.move(tmpDir, Paths.get(localPath));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
